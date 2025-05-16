be38ed52745ec5c8b4be4bf9d4d06778670e5c03
Veasna1 <veasnagva@gmial.com>
2025-05-16 20:35:33 +0700

    Fixing unknown credentail of inertia

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Inertia\Middleware;
+use Illuminate\Http\Request;
+
+class HandleInertiaRequests extends Middleware
+{
+    protected $rootView = 'app';
+
+    public function version(Request $request): ?string
+    {
+        return parent::version($request);
+    }
+
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+            'auth.user' => fn () => $request->user()
+                ? $request->user()->only('id', 'name', 'email')
+                : null,
+        ]);
+    }
+}
